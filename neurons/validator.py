f70b53c1e80856a618ed063e3255d6576d206252
salahawk <bstuqopavyrwku8251@gmail.com>
2023-11-17 07:20:12 +0000

    feat: store-retry

--- neurons/validator.py
+++ neurons/validator.py
@@ -37,10 +37,8 @@ from tqdm import tqdm
 import storage
 import allocate
 
-CHUNK_STORE_COUNT = 4
-CHUNK_SIZE = 1 << 20    # 1 MB
-MIN_N_CHUNKS = 1 << 10  # the minimum number of chunks a miner should provide at least is 1GB (CHUNK_SIZE * MIN_N_CHUNKS)
-TB_NAME = "saved_data"
+CHUNK_SIZE = 1 << 22    # 1 MB
+MIN_N_CHUNKS = 1 << 8  # the minimum number of chunks a miner should provide at least is 1GB (CHUNK_SIZE * MIN_N_CHUNKS)
 
 # Step 2: Set up the configuration parser
 # This function is responsible for setting up and parsing command-line arguments.
@@ -82,159 +80,6 @@ def get_config():
 
     # Return the parsed config.
     return config
-
-#MISSING
-
-#Create a database to store the given file
-def create_database_for_file(db_name):
-    db_base_path = f"{config.db_root_path}/{config.wallet.name}/{config.wallet.hotkey}/data"
-    if not os.path.exists(db_base_path):
-        os.makedirs(db_base_path, exist_ok=True)
-    conn = sqlite3.connect(f"{db_base_path}/{db_name}.db")
-    cursor = conn.cursor()
-    
-    cursor.execute(f"CREATE TABLE IF NOT EXISTS {TB_NAME} (chunk_id INTEGER PRIMARY KEY, miner_hotkey TEXT, miner_key INTEGER)")
-    conn.close()
-
-#Save the chunk(index : chunk_number) to db_name
-def save_chunk_location(db_name, chunk_number, store_resp_list):
-    conn = sqlite3.connect(f"{config.db_root_path}/{config.wallet.name}/{config.wallet.hotkey}/data/{db_name}.db")
-    cursor = conn.cursor()
-
-    for store_resp in store_resp_list:
-        cursor.execute(f"INSERT INTO {TB_NAME} (chunk_id, miner_hotkey, miner_key) VALUES (?, ?, ?)", (chunk_number, store_resp['hotkey'], store_resp['key']))
-    conn.commit()
-    conn.close()
-
-#Update the hash value of miner table
-def update_miner_hash(validator_hotkey, store_resp_list):
-    for store_resp in store_resp_list:
-        miner_hotkey = store_resp['hotkey']
-        db_path = f"{config.db_root_path}/{config.wallet.name}/{config.wallet.hotkey}/DB-{miner_hotkey}-{validator_hotkey}"
-        conn = sqlite3.connect(db_path)
-        cursor = conn.cursor()
-        update_request = f"UPDATE DB{miner_hotkey}{validator_hotkey} SET hash = ? where id = ?"
-        cursor.execute(update_request, (store_resp['hash'], store_resp['key']))
-        conn.commit()
-        conn.close()
-
-def hash_data(data):
-    hasher = hashlib.sha256()
-    hasher.update(data)
-    return hasher.digest()
-
-#Retrieve the file
-def retrieve_file(metagraph, dendrite, validator_hotkey, db_name):
-    output_filename = "retrieved_" + db_name.rsplit('_', 1)[0]
-    db_path = f"{config.db_root_path}/{config.wallet.name}/{config.wallet.hotkey}/data/{db_name}.db"
-    conn = sqlite3.connect(db_path)
-    cursor = conn.cursor()
-
-    cursor.execute(f"SELECT * FROM {TB_NAME}")
-    rows = cursor.fetchall()
-
-    chunk_size = max(rows, key=lambda obj: obj[0])[0] + 1
-    
-    hotkey_axon_dict = {}
-    for axon in metagraph.axons:
-        hotkey_axon_dict[axon.hotkey] = axon
-
-    with open(output_filename, 'wb') as output_file:
-        for id in range(chunk_size):
-            cursor.execute(f"SELECT * FROM {TB_NAME} where chunk_id = {id}")
-            rows = cursor.fetchall()
-            hotkey_list = [row[1] for row in rows]
-            key_list = {row[1]:row[2] for row in rows}
-            axons_list = [hotkey_axon_dict[hotkey] for hotkey in hotkey_list]
-
-            miner_hotkey = axons_list[0].hotkey
-            db = sqlite3.connect( f"{config.db_root_path}/{config.wallet.name}/{config.wallet.hotkey}/DB-{miner_hotkey}-{validator_hotkey}")
-            validation_hash = (
-                db.cursor()
-                .execute(
-                    f"SELECT hash FROM DB{miner_hotkey}{validator_hotkey} WHERE id=?", (key_list[miner_hotkey],)
-                )
-                .fetchone()[0]
-            )
-            db.close()
-            
-            retrieve_response = dendrite.query(
-                axons_list,
-                storage.protocol.Retrieve(key_list = key_list),
-                deserialize=True,
-            )
-            chunk_data = ''
-            for index, retrieve_resp in enumerate(retrieve_response):
-                if retrieve_resp and hash_data(retrieve_resp.encode('utf-8')) == validation_hash:
-                    chunk_data = retrieve_resp
-                    break
-            if not chunk_data:
-                bt.logging.info(f"Chunk_{id} is missing!")
-            else:
-                hex_representation = chunk_data.split("'")[1]
-                clean_hex_representation = ''.join(c for c in hex_representation if c in '0123456789abcdefABCDEF')
-                # Convert the cleaned hexadecimal representation back to bytes
-                chunk_data = bytes.fromhex(clean_hex_representation)
-                output_file.write(chunk_data)
-    conn.close()
-
-#Store the provided file
-def store_file(metagraph, dendrite, validator_hotkey, file_path, chunk_size):
-    file_name = os.path.basename(file_path)
-
-    db_name = file_name + "_" + str(int(time.time()))
-    create_database_for_file(db_name)
-    #Number of miners
-    axon_count = len(metagraph.axons)
-    with open(file_path, 'rb') as infile:
-        chunk_number = 0
-        while True:
-            chunk = infile.read(chunk_size)
-            if not chunk:
-                break  # reached end of file
-            hex_representation = ''.join([f'\\x{byte:02x}' for byte in chunk])
-
-            # Construct the desired string
-            chunk = f"b'{hex_representation}'"
-            
-            #Generate list of miners who will receive chunk, count: CHUNK_STORE_COUNT
-            index_list = []
-            for i in range(CHUNK_STORE_COUNT):
-                while True:
-                    chunk_i = random.randint(0, axon_count - 1)
-                    if chunk_i in index_list:
-                        continue
-                    index_list.append(chunk_i)
-                    break
-            
-            #Transfer the chunk to selected miners
-            axons_list = []
-            for index in index_list:
-                axons_list.append(metagraph.axons[index])
-
-            store_response = dendrite.query(
-                axons_list,
-                storage.protocol.Store(data = chunk),
-                deserialize=True,
-            )
-            
-            store_resp_list = []
-
-            for index, key in enumerate(store_response):
-                if key != -1: #Miner saved the chunk
-                    store_resp_list.append({"key": key, "hotkey": axons_list[index].hotkey, "hash": hash_data(chunk.encode('utf-8'))})
-
-            if not store_resp_list:
-                return {"status": False, "error_msg" : "NOT ENOUGH SPACE"}
-            
-            #Save the key to db
-            save_chunk_location(db_name, chunk_number, store_resp_list)
-            
-            #Update the hash value of the key that miner responded
-            update_miner_hash(validator_hotkey, store_resp_list)
-
-            chunk_number += 1
-    return {"status": True, "db_name":db_name}
 
 def main(config):
     # Set up logging with the provided configuration and directory.
@@ -317,7 +162,7 @@ def main(config):
         workers=10,  # The number of concurrent workers to use for generation. Default is 10.
         restart=True,  # Dont restart the generation from empty files.
     )
-    
+
     # Step 7: The Main Validation Loop
     bt.logging.info("Starting validator loop.")
     step = 0
@@ -326,11 +171,10 @@ def main(config):
             # Iterate over all miners on the network and validate them.
             previous_allocations = copy.deepcopy(next_allocations)
             for i, alloc in tqdm(enumerate(next_allocations)):
-                bt.logging.debug(f"Starting")
                 # Dont self validate.
                 if alloc["miner"] == wallet.hotkey.ss58_address:
                     continue
-                bt.logging.debug(f"Validating miner [uid {i}]: {alloc}")
+                bt.logging.debug(f"Validating miner [uid {i}]")
 
                 # Select a random chunk to validate.
                 verified_n_chunks = verified_allocations[i]["n_chunks"]
@@ -339,7 +183,7 @@ def main(config):
                     chunk_i = str(random.randint(0, new_n_chunks - 1))
                 else:
                     chunk_i = str(random.randint(verified_n_chunks, new_n_chunks - 1))
-                bt.logging.debug(f"Validating chunk: {chunk_i}")
+                bt.logging.debug(f"Validating chunk_{chunk_i}")
 
                 # Get the hash of the data to validate from the database.
                 db = sqlite3.connect(alloc["path"])
@@ -353,10 +197,9 @@ def main(config):
                     )
                 except:
                     bt.logging.error(
-                        f"Failed to get validation hash for chunk: {chunk_i} from db: {alloc['path']}"
+                        f"❌ Failed to get validation hash for chunk_{chunk_i}"
                     )
                     continue
-                bt.logging.debug(f"Validation hash: {validation_hash}")
                 db.close()
 
                 # Query the miner for the data.
@@ -377,15 +220,12 @@ def main(config):
                         verified_allocations[i]["n_chunks"],
                     )
                     bt.logging.debug(
-                        f"Miner [uid {i}] did not respond with data, reducing allocation to: {next_allocations[i]['n_chunks']}"
+                        f"💤 Miner [uid {i}] did not respond with data, reducing allocation to: {next_allocations[i]['n_chunks']}"
                     )
 
                 else:
                     # The miner was able to respond with the data, but we need to verify it.
                     computed_hash = hashlib.sha256(miner_data.encode()).hexdigest()
-                    bt.logging.debug(
-                        f"Miner [uid {i}] Computed hash: {computed_hash}, Validation hash: {validation_hash} "
-                    )
 
                     # Check if the miner has provided the correct response by doubling the dummy input.
                     if computed_hash == validation_hash:
@@ -398,7 +238,7 @@ def main(config):
                             next_allocations[i]["n_chunks"] * 1.1
                         )
                         bt.logging.debug(
-                            f"Miner [uid {i}] provided correct response, increasing allocation to: {next_allocations[i]['n_chunks']}"
+                            f"✅ Miner [uid {i}] provided correct response, increasing allocation to: {next_allocations[i]['n_chunks']}"
                         )
                     else:
                         # The miner has provided an incorrect response.
@@ -411,20 +251,13 @@ def main(config):
                             verified_allocations[i]["n_chunks"],
                         )
                         bt.logging.debug(
-                            f"Miner [uid {i}] provided incorrect response, reducing allocation to: {next_allocations[i]['n_chunks']}"
+                            f"👎 Miner [uid {i}] provided incorrect response, reducing allocation to: {next_allocations[i]['n_chunks']}"
                         )
 
-            # Reallocate the validator's chunks.
-            bt.logging.debug(
-                f"Prev allocations: {[ a['n_chunks'] for a in previous_allocations ]  }"
-            )
             allocate.generate(
                 allocations=next_allocations,  # The allocations to generate.
                 no_prompt=True,  # If True, no prompt will be shown
                 restart=False,  # Dont restart the generation from empty files.
-            )
-            bt.logging.info(
-                f"Allocations: {[ allocate.human_readable_size( a['n_chunks'] * allocate.CHUNK_SIZE ) for a in next_allocations ] }"
             )
 
             # Periodically update the weights on the Bittensor blockchain.
@@ -442,9 +275,9 @@ def main(config):
                     wait_for_inclusion=True,
                 )
                 if result:
-                    bt.logging.success("Successfully set weights.")
+                    bt.logging.success("✅ Successfully set weights.")
                 else:
-                    bt.logging.error("Failed to set weights.")
+                    bt.logging.error("❌ Failed to set weights.")
 
             # End the current step and prepare for the next iteration.
             step += 1
